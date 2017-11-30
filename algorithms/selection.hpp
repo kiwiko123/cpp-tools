cc3d6bd0bd8780d5e7b443f23663ddd900f74706
Geoffrey Ko <kogw@uci.edu>
2017-11-30 15:34:17 -0800

    initial commit of deterministic selection; generic select function

--- algorithms/selection.hpp
+++ algorithms/selection.hpp
@@ -12,6 +12,7 @@
 #ifndef ALGORITHMS_SELECTION_HPP
 #define ALGORITHMS_SELECTION_HPP
 
+#include <array>
 #include <deque>
 #include <iostream>
 #include <iterator>
@@ -31,6 +32,28 @@ namespace
         {
             throw std::out_of_range{"empty range"};
         }
+    }
+
+    template <typename InputIterator>
+    int brute_force_median(InputIterator first, InputIterator last)
+    {
+        std::vector<int> temp{first, last};
+        std::sort(temp.begin(), temp.end());
+        int offset = std::floor(temp.size() / 2);
+        int median = temp[offset];
+        if (temp.size() % 2 == 0)
+        {
+            median = (median + temp[offset + 1]) / 2;
+        }
+        return median;
+    }
+
+    template <typename InputIterator>
+    int brute_force_select(InputIterator first, InputIterator last, int k)
+    {
+        std::vector<int> temp{first, last};
+        std::sort(temp.begin(), temp.end());
+        return temp[k];
     }
 }
 
@@ -65,8 +88,9 @@ int find_maximum(InputIterator first, InputIterator last)
  * Using a tournament tree, the second-largest element is one of the elements that was directly compared with the maximum.
  * By building a bottom-up heap to represent the comparisons, we can:
  *   - find the maximum value using n - 1 comparisons
- *   - find the second-largest value in O(log n) time
- *   - find the second-largest value by searching the tournament-tree using ⌈log n⌉ - 1 comparisons.
+ *   - find the second-largest value:
+ *     - in Θ(log n) time (by definition, heaps are balanced)
+ *     - using Θ(⌈log n⌉ - 1) comparisons.
  *
  * Example: [64, 41, 53, 62, 68, 60, 75, 63]
  * build tournament tree:
@@ -80,11 +104,14 @@ int find_maximum(InputIterator first, InputIterator last)
  * 64 41 53 62 68 60 75 63
  *
  * Build the tree bottom-up - the leaves are each pair of numbers:
- * (64, 41), (53, 62), (68, 60), (75, 63)
+ *   (64, 41), (53, 62), (68, 60), (75, 63)
  *
- * Select the "winners" (greater numbers) of each pair.
+ * Select the "winners" (greater numbers) of each pair:
+ *   (64, 62), (68, 75)
+ *
  * Continue doing this until the entire tree is built.
- * Descend the tree in O(log n) time to find the maximum value that 75 was compared to.
+ * Descend the tree in Θ(log n) time (considering only nodes of which 75 is a child)
+ * to find the maximum value that 75 was compared to.
  * This incurs Θ(⌈log n⌉ - 1) comparisons.
  */
 template <typename InputIterator>
@@ -146,9 +173,46 @@ int find_second_largest(InputIterator first, InputIterator last)
 }
 
 
+template <typename InputIterator, typename Selector>
+int select_k(InputIterator first, InputIterator last, int k, int median, 
+             std::vector<int>& less, std::vector<int>& equal, std::vector<int>& greater,
+             const Selector& selector)
+{
+    while (first != last)
+    {
+        if (*first < median)
+        {
+            less.push_back(*first);
+        }
+        else if (*first > median)
+        {
+            greater.push_back(*first);
+        }
+        else
+        {
+            equal.push_back(*first);
+        }
+        ++first;
+    }
+
+    if (k <= less.size())
+    {
+        return selector(less.begin(), less.end(), k);
+    }
+    else if (k <= (less.size() + equal.size()))
+    {
+        return median;
+    }
+    else
+    {
+        return selector(greater.begin(), greater.end(), k - less.size() - equal.size());
+    }
+}
+
+
 /* Quick select.
  * Prune-and-search algorithm for finding the k-th smallest value in the iterator range [first, last).
- * Choose and approximate median "m*" randomly from [first, last).
+ * Choose an approximate median "m*" randomly from [first, last).
  * Let S be the sequence of items from [first, last).
  * Let L be the sequence of items in S that are less than m*.
  * Let E be the sequence of items in S that are equal to m*.
@@ -207,6 +271,77 @@ int quick_select(InputIterator first, InputIterator last, int k)
     else
     {
         return quick_select(greater.begin(), greater.end(), k - less.size() - equal.size());
+    }
+}
+
+
+template <typename InputIterator>
+int deterministic_select(InputIterator first, InputIterator last, int k)
+{
+    int size = std::distance(first, last);
+    if (size <= 5)
+    {
+        return brute_force_select(first, last, k);
+    }
+
+    const double group_factor = 5.0;
+    const int n_groups = std::ceil(size / group_factor);
+    const double median_of_n_groups = n_groups / static_cast<double>(2);
+    std::vector<int> medians(n_groups);
+    std::vector<std::vector<int>> groups(n_groups);
+    std::for_each(groups.begin(), groups.end(), [&group_factor](std::vector<int>& group){ group = std::vector<int>(group_factor); });
+
+    // 1. divide into ⌈n/5⌉ groups
+    auto current = first;
+    for (unsigned int i = 0; i < size; ++i)
+    {
+        int group_factor_i = group_factor;
+        groups[std::floor(i / group_factor)][i % group_factor_i] = *(current++);
+    }
+
+    // 2. find the medians of each group, again using brute force
+    for (unsigned int i = 0; i < n_groups; ++i)
+    {
+        const std::vector<int>& target = groups[i];
+        medians[i] = brute_force_select(target.begin(), target.end(), std::floor(median_of_n_groups));
+    }
+
+    // 3. compute m* (median-of-medians)
+    int median_of_medians = deterministic_select(medians.begin(), medians.end(), std::floor(median_of_n_groups));
+
+    // 4. parition into L, E, G
+    std::vector<int> less;
+    std::vector<int> equal;
+    std::vector<int> greater;
+
+    while (first != last)
+    {
+        if (*first < median_of_medians)
+        {
+            less.push_back(*first);
+        }
+        else if (*first > median_of_medians)
+        {
+            greater.push_back(*first);
+        }
+        else
+        {
+            equal.push_back(*first);
+        }
+        ++first;
+    }
+
+    if (k <= less.size())
+    {
+        return deterministic_select(less.begin(), less.end(), k);
+    }
+    else if (k <= (less.size() + equal.size()))
+    {
+        return median_of_medians;
+    }
+    else
+    {
+        return deterministic_select(greater.begin(), greater.end(), k - less.size() - equal.size());
     }
 }
 
